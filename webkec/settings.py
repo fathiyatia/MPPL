6892bf3c8ccce748f9445cb4dccf56a564be5b0f
Yasmin <Yasmin_bwzr@apps.ipb.ac.id>
2020-12-22 17:21:54 +0700

    debugging

--- webkec/settings.py
+++ webkec/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(ox(@q0toob!&1hx$g71uqfabdhncjtx%f7f3ah*(e92aj74sl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
