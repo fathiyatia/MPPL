699257c516f5e0362191da8326df42005c19d6b5
Yasmin <Yasmin_bwzr@apps.ipb.ac.id>
2020-12-22 17:37:02 +0700

    debugging

--- webkec/settings.py
+++ webkec/settings.py
@@ -161,6 +161,8 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/documents')
 
 MEDIA_URL = '/documents/' 
